53d903278bd80aa30a512417b2ee920aec2158dd
DarwinKopimi <dadaist92@gmail.com>
2021-07-03 22:09:27 -0400

    spring project

---
+++ WhiskeySinister/whiskey-sinister/src/main/java/com/whiskey/webservice/whiskeysinister/WhiskeySinisterApplication.java
@@ -0,0 +1,13 @@
+package com.whiskey.webservice.whiskeysinister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeySinisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskeySinisterApplication.class, args);
+	}
+
+}
