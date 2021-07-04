f6c8c04e2e155294dd88b1ac0212f3f9dc3d15b9
DarwinKopimi <dadaist92@gmail.com>
2021-07-04 12:20:52 -0400

    redid file with jersey rest file

---
+++ WhiskeySinister/sinister/src/main/java/com/whiskey/sinister/SinisterApplication.java
@@ -0,0 +1,13 @@
+package com.whiskey.sinister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinisterApplication.class, args);
+	}
+
+}
